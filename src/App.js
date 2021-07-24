3918a6d03abf223c09a6df948031e9087f065d0f
InbalEl <inbalelcode@gmail.com>
2021-07-24 16:56:25 +0300

    background added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import UrgeWithPleasureComponent from './components/Timer/Timer';
 function App() {
   return (
     <div className="Container">
-      <h1>zenMode</h1>
-      <small>Level up your productivity</small>
+      <h1 className='zenMode'>zenMode</h1>
+      <p className='levelUp'>Level up your productivity</p>
       <CurrentStatus/>
-      <UrgeWithPleasureComponent/>
+      <UrgeWithPleasureComponent className='timerCircle' timerDuration='2'/>
     </div>
   );
 }
--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -5,18 +5,20 @@ const renderTime = ({ remainingTime }) => {
   
     return (
       <div className="timerInnerContent">
-        <div className="value">{remainingTime}</div>
+        <div className="value">{Math.ceil(remainingTime / 60)}</div>
+        <div className="text">minutes</div>
+        <div className="value">{remainingTime % 60}</div>
         <div className="text">seconds</div>
       </div>
     );
   };
   
 
-const UrgeWithPleasureComponent = () => (
+const UrgeWithPleasureComponent = (props) => (
     <div>
     <CountdownCircleTimer
     isPlaying
-        duration={10}
+        duration={props.timerDuration * 60}
         colors={[
             ['#0c75b2', 0.33],
             ['#4a9ecf', 0.33],
